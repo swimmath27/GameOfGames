b7ad0d524b8075edd786f055e2926a1a9cd7c034
Michael Lombardo <swimmath27@gmail.com>
2016-05-31 14:23:47 -0400

    Initial Commit

---
+++ GameOfGames/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  GameOfGames
+//
+//  Created by Michael Lombardo on 5/31/16.
+//  Copyright © 2016 Michael Lombardo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
