3ce5a0b46e7dd1ebb6b8fbf8a03526b8fa98da35
Michael Lombardo <swimmath27@gmail.com>
2017-05-01 23:21:03 -0400

    Moved win condition to after round roulette

--- GameOfGames/ViewControllers/PlayGameViewController.swift
+++ GameOfGames/ViewControllers/PlayGameViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class PlayGameViewController: UIViewController {
-  
+
+  // True when round roulette has just been played and we're
+  // back in this controller to actually start the new round
   static fileprivate var wasNewRound:Bool = true;
   
   @IBOutlet weak var roundLabel: UILabel!
@@ -54,70 +56,116 @@ class PlayGameViewController: UIViewController {
     messageTitleLabel.text = game.messageTitle;
     messageLabel.text = game.message;
     
-    if (game.isNewRound()) {
-      if !PlayGameViewController.wasNewRound {
-        // it's actually a new round so we need to display the previous round cards
-        Team1CardsLabel.text = "\(game.getTeamCardsInLastRound(1)) Cards";
-        Team2CardsLabel.text = "\(game.getTeamCardsInLastRound(2)) Cards";
+    if (self.timeForRoundRoulette()) {
+      // it's actually a "new round" but we need to display
+      // the previous round cards before round roulette
+      Team1CardsLabel.text =
+          "\(game.getTeamCardsInLastRound(1)) Cards";
+      Team2CardsLabel.text =
+          "\(game.getTeamCardsInLastRound(2)) Cards";
 
-        // Reuse the "player 1, you're up next" labels
-        nextPlayerLabel.textAlignment = .center;
-        nextPlayerLabel.text = "End of Round, Click to"
-        upNextLabel.text = "go to Round Roulette"
+      // reusing the labels
+      nextPlayerLabel.textAlignment = .center;
+      nextPlayerLabel.text = "End of Round, Tap to"
+      upNextLabel.text = "go to Round Roulette"
 
-        drawCardButton.setImage(UIImage(named:"rollButton.png"), for: UIControlState());
-      }
+      drawCardButton.setImage(
+          UIImage(named: "rollButton.png"),
+          for: UIControlState());
     }
     else {
-      PlayGameViewController.wasNewRound = false
-      
-      if game.getCurrentTeam() == 1  {  // team 2 is up 
+      // normal gameplay
+      if game.isNewRound() {  // Just got back from round roulette
+        // no win message right now
+        messageTitleLabel.text = ""
+        messageLabel.text = ""
+
+        // check for a win condition
+        let whoWon: Int = self.determineAWinner();
+        if (whoWon == 1) {
+          drawCardButton.isHidden = true;
+          nextPlayerLabel.text = "";
+          upNextLabel.text = "\(game.getTeamName(1)) Win!"
+        } else if (whoWon == 2) {
+          drawCardButton.isHidden = true;
+          nextPlayerLabel.text = "";
+          upNextLabel.text = "\(game.getTeamName(2)) Win!"
+        }
+        else if !game.hasNextCard() {
+          drawCardButton.isHidden = true;
+          upNextLabel.text = "Deck is Out of Cards!"
+        
+          //reusing these labels...
+          nextPlayerLabel.textAlignment = .center;
+          if (game.getTeam1Score() > game.getTeam2Score()) {
+            nextPlayerLabel.text =
+                "\(game.getTeamName(1)) Win!";
+          }
+          else if (game.getTeam1Score() < game.getTeam2Score()) {
+            nextPlayerLabel.text =
+                "\(game.getTeamName(2)) Win!";
+          }
+          else {
+            nextPlayerLabel.text = "Tie Game!";
+          }
+        }  // !game.hasNextCard
+      }  // game.isNewRound
+      else {
+        // not a new round anymore
+        PlayGameViewController.wasNewRound = false
+      }
+
+      // make current team's font larger
+      if game.getCurrentTeam() == 1  {  // team 1 is up
         team1NameLabel.font = UIFont(descriptor: team1NameLabel.font.fontDescriptor, size: 27)
         team2NameLabel.font = UIFont(descriptor: team1NameLabel.font.fontDescriptor, size: 22)
       }
-      else  {  // team 2 is up 
+      else  {  // team 2 is up
         team1NameLabel.font = UIFont(descriptor: team1NameLabel.font.fontDescriptor, size: 22)
         team2NameLabel.font = UIFont(descriptor: team1NameLabel.font.fontDescriptor, size: 27)
       }
-      
-      //check if game is over
-      if (game.getTeam1Score() >= Game.NUM_CARDS_TO_WIN) {
-        drawCardButton.isHidden = true;
-        nextPlayerLabel.text = "";
-        upNextLabel.text = "\(game.getTeamName(1)) Win!"
-      }
-      else if (game.getTeam2Score() >= Game.NUM_CARDS_TO_WIN) {
-        drawCardButton.isHidden = true;
-        nextPlayerLabel.text = "";
-        upNextLabel.text = "\(game.getTeamName(2)) Win!"
-      }
-      else if !game.hasNextCard() {
-        drawCardButton.isHidden = true;
-        nextPlayerLabel.textAlignment = .center;
-        upNextLabel.text = "Deck is Out of Cards!"
-        
-        //reusing these labels...
+
+    } // normal play
+    self.view.layer.insertSublayer(UIHelper.getBackgroundGradient(), at: 0)
+  }  // ViewDidLoad
+
+  // TODO: replace this with an enum?
+  // team1won, team2won, tiedWinConditions, NoWinner
+  // display something like "The game is still tied, continuing..."
+  // under tiedWinConditions
+  func determineAWinner() -> Int {
+    if (game.getTeam1Score() >= Game.NUM_CARDS_TO_WIN) {
+      if (game.getTeam2Score() >= Game.NUM_CARDS_TO_WIN) {
+        // both teams could win
         if (game.getTeam1Score() > game.getTeam2Score()) {
-          nextPlayerLabel.text = "Team 1 (\(game.getTeamName(1))) Wins!";
-          
+          // team 1 won
+          return 1
         }
-        else if (game.getTeam1Score() < game.getTeam2Score()) {
-          
-          nextPlayerLabel.text = "Team 2 (\(game.getTeamName(2))) Wins!";
+        else if (game.getTeam2Score() > game.getTeam1Score()) {
+          // team 2 won
+          return 2
         }
-        else {
-          nextPlayerLabel.text = "Tie Game!";
-        }
-        
+        //else {}  // tied win conditions
+      }
+      else {
+        // team 1 won
+        return  1;
       }
     }
-    
-    
-    self.view.layer.insertSublayer(UIHelper.getBackgroundGradient(), at: 0)
+    else if (game.getTeam2Score() >= Game.NUM_CARDS_TO_WIN) {
+      return 2;
+    }
+    // no outright winner
+    return 0;
+  }
+
+  func timeForRoundRoulette() -> Bool{
+    return game.isNewRound() &&
+      !PlayGameViewController.wasNewRound;
   }
 
   @IBAction func drawCardButtonPressed(_ sender: AnyObject) {
-    if (game.isNewRound() && !PlayGameViewController.wasNewRound) {
+    if (self.timeForRoundRoulette()) {
       PlayGameViewController.wasNewRound = true;
       performSegue(withIdentifier: "PlayGameToRoundRoulette", sender: nil)
     }
